c086948259801f617f2f63b23d707f29b855f447
Krathish sk <krathishs.k123@gmail.com>
2023-01-19 09:58:03 +0530

    added invaders

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 const grid = document.querySelector(".grid");
 
-for (let i = 0; i < 255; i++) {
-  const squares = document.createElement("div");
-  grid.appendChild(squares);
+for (let i = 0; i < 225; i++) {
+  const square = document.createElement("div");
+  grid.appendChild(square);
 }
+
+//Create sqaures array
+
+const squares = Array.from(document.querySelectorAll(".grid div"));
+
+// Create Invaders
+const alienInvaders = [
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 30, 31,
+  32, 33, 34, 35, 36, 37, 38, 39,
+];
+
+//Functions add invaders
+function draw() {
+  for (let i = 0; i < alienInvaders.length; i++) {
+    squares[alienInvaders[i]].classList.add("invader");
+  }
+}
+
+draw();
